2cce1317149f59f9610f10dfbd9d1d0f753d08a5
unknown <ghilesyal99@gmail.com>
2025-11-17 18:30:35 +0100

    Profile update

--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from .forms import UserSignUpForm
+from .forms import UserUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+class UserUpdateView(UserPassesTestMixin, UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    success_url = reverse_lazy("accounts:user-profile")
+    template_name = "user/update.html"
+
+    def test_func(self):
+        pkInURL = self.kwargs['pk']
+        return self.request.user.is_authenticated and self.request.user.id==pkInURL or self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        messages.error(self.request, "Vous n'avez pas l'autorisation d'accéder à cette page!")
+        return redirect('accounts:user-profile')
 
 
 class UserSignUpView(UserPassesTestMixin, CreateView):
